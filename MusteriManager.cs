6d156f82ccdc046093783a97b81ee5c261af06e0
Emrecabuk1987 <pentagonmc1@gmail.com>
2021-01-22 01:00:06 +0200

    16/01/2021-odev

---
+++ MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+
+    class MusteriManager
+    {
+        public void Ekle()
+        {
+            Console.WriteLine("Yeni Musteri Eklendi");
+
+
+        }
+
+        public void Sil()
+        {
+
+            Console.WriteLine("Musteri Başarıyla Sistemden Silindi");
+        }
+
+    }
+
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Musteri musteri = new Musteri();
+
+            musteri.MusteriAdi = "Emre";
+            musteri.MusteriSoyadi = "Çabuk";
+            musteri.Id = 103167813;
+            musteri.DogumTarihi = 1987;
+
+            Musteri[] Musteriler = new Musteri[] { musteri };
+
+            foreach (var musteri1 in Musteriler)
+
+            {
+                Console.WriteLine("Musteri Adı : " + musteri.MusteriAdi + "\nMusteri Soyadı : " + musteri.MusteriSoyadi + "\nDoğum Tarihi : " + musteri.DogumTarihi + "\nMusteri İd Numarası " + musteri.Id);
+
+            }
+
+            MusteriManager musterimanager = new MusteriManager();
+            musterimanager.Ekle();
+
+            MusteriManager Musterimanager = new MusteriManager();
+            Musterimanager.Sil();
+       
+        }
+
+
+
+        class Musteri
+        {
+
+            public string MusteriAdi { get; set; }
+            public string MusteriSoyadi { get; set; }
+            public int Id { get; set; }
+            public int DogumTarihi { get; set; }
+
+        }
+
+
+
+        
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+    }
+}
